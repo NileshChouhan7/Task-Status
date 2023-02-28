6ae6f7974ded1e5d97b335e3afe6a940b0892236
NileshChouhan7 <mrnileshchouhan6>
2023-02-28 13:19:30 +0530

    Initial Commit

---
+++ src/main/java/com/java/taskstatus/TaskStatusApplication.java
@@ -0,0 +1,13 @@
+package com.java.taskstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/taskstatus/TaskStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.taskstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
